4e8cf03565266ae5865797e9c661cfcd1abeab90
Angie997 <angiielunaa97@gmail.com>
2025-01-25 18:47:47 -0300

    Actualizacion del archivo carteras.js

--- carteras.js
+++ carteras.js
@@ -1,8 +1,9 @@
-
+// Lista de carteras disponibles
 const carterasDisponibles = ["Cartera Roja", "Cartera Negra", "Cartera Azul", "Cartera Blanca"];
 let carrito = [];
 let continuar = true;
 
+// Función para mostrar las carteras disponibles
 function mostrarCarteras() {
   console.log("Carteras disponibles:");
   carterasDisponibles.forEach((cartera, index) => {
@@ -10,6 +11,7 @@ function mostrarCarteras() {
   });
 }
 
+// Función para agregar una cartera al carrito
 function agregarCarteraAlCarrito() {
   let seleccion = parseInt(prompt("Selecciona el número de la cartera que deseas comprar (1-4):"));
   if (seleccion >= 1 && seleccion <= carterasDisponibles.length) {
@@ -21,6 +23,7 @@ function agregarCarteraAlCarrito() {
   }
 }
 
+// Función para mostrar el contenido del carrito
 function mostrarCarrito() {
   if (carrito.length > 0) {
     alert(`Tu carrito contiene: \n${carrito.join("\n")}`);
@@ -29,6 +32,7 @@ function mostrarCarrito() {
   }
 }
 
+// Función para iniciar el simulador de compras
 function iniciarSimulador() {
   alert("Bienvenido a la tienda Angelic Carteras.");
   while (continuar) {
@@ -36,8 +40,15 @@ function iniciarSimulador() {
     agregarCarteraAlCarrito();
     continuar = confirm("¿Deseas comprar otra cartera?");
   }
-  mostrarCarrito();
-  alert("Gracias por tu compra. ¡Vuelve pronto!");
+
+  if (carrito.length > 0) {
+    mostrarCarrito();
+    alert("Gracias por tu compra. ¡Vuelve pronto!");
+  } else {
+    alert("No realizaste ninguna compra. ¡Vuelve pronto!");
+  }
 }
 
+// Iniciar el simulador de compras
 iniciarSimulador();
+
